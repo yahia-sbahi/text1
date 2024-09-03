d6f373f458269d612392343a1b7a8aa78b9c14ad
unknown <yahiasbahi1@gmail.com>
2024-09-04 00:50:19 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,200 @@
+"use client";
 import Image from "next/image";
+import vercel from "../public/bike-svgrepo-com.svg";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface Bikes {
+  id: number;
+  date_stolen: number;
+  description: string;
+  large_img: string;
+  title: string;
+  year: number;
+  stolen_location: string;
+}
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [search, setSearch] = useState("");
+  const abc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [startDate, setStartDate] = useState(abc);
+  const rowsPerPage = 10;
+  const [startIndex, setStartIndex] = useState(1);
+  const [endIndex, setEndIndex] = useState(rowsPerPage);
+  const [data, setData] = useState<Bikes[]>([]);
+  const formatDate = (timestamp: number) =>
+    new Date(timestamp * 1000).toLocaleDateString();
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://bikeindex.org:443/api/v3/search?page=${startIndex}&per_page=${rowsPerPage}&distance=10&stolenness=stolen&query=${search}&access_token=eJW36PlqR9EmKWTEm0fm_iTrmL_wEk6FaBg78fIpg48`
+      );
+      const newresponse = await axios.get(
+        `https://bikeindex.org:443/api/v3/search/count?location=IP&distance=10&stolenness=stolen&access_token=eJW36PlqR9EmKWTEm0fm_iTrmL_wEk6FaBg78fIpg48
+`
+      );
+      const responseData = response.data.bikes;
+      const total = newresponse.data.stolen;
+      setData(responseData);
+      setTotal(total);
+      console.log(data);
+      console.log(total);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, [startIndex]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="min-h-screen">
+        <main className="p-4 flex flex-col items-center justify-center w-full ">
+          <span className="text-4xl font-bold mt-5">Bikes</span>
+          <div className="bg-black text-white font-bold p-5 my-3 rounded-xl  flex flex-col space-y-2 justify-center items-center">
+            <p>total number of stolen bikes {total}</p>
+            <div className=" flex justify-center items-center">
+              <input
+                className=" p-4 text-black outline-none rounded-lg"
+                type="text"
+                placeholder="Search by title"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </div>
+          </div>
+          <button
+            className="bg-black text-white rounded-full p-5"
+            onClick={() => getData()}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            search
+          </button>
+
+          <div className=" mt-4 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {loading &&
+              Array.isArray(startDate) &&
+              startDate.map((id) => (
+                <Card
+                  key={id}
+                  className="flex flex-col justify-between shadow-2xl"
+                >
+                  <CardHeader>
+                    <CardTitle>
+                      <Skeleton className=" w-5 h-5" />
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent className="  items-center justify-center flex flex-col">
+                    <div className="relative h-40 w-60">
+                      <Skeleton className=" h-40 w-full" />
+                    </div>
+                  </CardContent>
+                  <CardFooter className="flex flex-col items-center text-center space-y-1  justify-center ">
+                    <Skeleton className="w-14 h-4" />
+                    <Skeleton className="w-14 h-4" />
+                    <Skeleton className="w-14 h-4" />
+                  </CardFooter>
+                </Card>
+              ))}
+          </div>
+          <div className=" mt-4 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {!loading &&
+              data &&
+              data.length > 0 &&
+              data.map((bike) => (
+                <Card
+                  key={bike.id}
+                  className="flex flex-col justify-between shadow-2xl"
+                >
+                  <CardHeader>
+                    <CardTitle>{bike.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="  items-center justify-center flex flex-col">
+                    <div className="relative h-40 w-full">
+                      {bike.large_img ? (
+                        <Image
+                          src={bike.large_img}
+                          alt={bike.title}
+                          fill
+                          className=" rounded-md"
+                        />
+                      ) : (
+                        <Image
+                          src={vercel}
+                          alt={bike.title}
+                          fill
+                          className=" rounded-md"
+                        />
+                      )}
+                    </div>
+                  </CardContent>
+                  <CardFooter className="flex flex-col items-center text-center  justify-center ">
+                    <p className=" text-red-600">
+                      Theft Date:{formatDate(bike.date_stolen)}
+                    </p>
+                    <p className="text-blue-600">Reported Date:{bike.year} </p>
+                    <p>Location: {bike.stolen_location}</p>
+                    <CardDescription className=" text-sm font-medium p-2 text-center">
+                      {bike.description}
+                    </CardDescription>
+                  </CardFooter>
+                </Card>
+              ))}
+          </div>
+        </main>
+      </div>
+      <Pagination className="">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              className={
+                startIndex === 1 ? "pointer-events-none opacity-50" : undefined
+              }
+              onClick={() => {
+                setStartIndex(startIndex - 1);
+              }}
             />
-          </a>
-        </div>
-      </div>
+          </PaginationItem>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <PaginationItem>
+            <PaginationNext
+              className={
+                endIndex === 1100 ? "pointer-events-none opacity-50" : undefined
+              }
+              onClick={() => {
+                setStartIndex(startIndex + 1); //10
+              }}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+      <footer className="p-4 text-white text-sm sm:text-lg bg-black flex justify-center items-center  w-full">
+        {" "}
+        &copy; 2024 <span className=" p-1 font-bold">Bikes </span>All right
+        reserved
+      </footer>
+    </>
   );
 }
